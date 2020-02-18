6c28043eb01c3756529867e43790f940a31247b3
yueyuegd <zy090631@163.com>
2020-02-18 16:19:45 +0800

    Default Changelist

---
+++ src/main/java/jvm/TestClass.java
@@ -0,0 +1,18 @@
+package jvm;
+
+/**
+ * @author zhangyue95
+ * @date 2020/2/10.
+ * 用于学习class文件的简单demo
+ */
+public class TestClass {
+
+    private int m;
+    private int inc() {
+        return m + 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
